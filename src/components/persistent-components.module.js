27bf94e1f500836964eb092649b8f4abc4e0d701
Jens Ungerer <github@jens-ungerer.de>
2020-01-25 02:12:41 +0100

    first version of setting up the logic for the minesweeper-game

--- src/components/persistent-components.module.js
+++ src/components/persistent-components.module.js
@@ -2,9 +2,11 @@ import angular from 'angular';
 import config from '../common/config';
 
 import navigation from './navigation/navigation.component';
+import minesweeperGame from './../view/minesweeper-game/minesweeper-game.component';
 
 export default angular
-  .module(`${config.appName}.persistentComponentsr`, [
-    navigation
+  .module(`${config.appName}.persistentComponents`, [
+    navigation,
+    minesweeperGame
   ])
   .name;
